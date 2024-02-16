e9dd2f607978c097e871e10b62478975dd8828ae
Snehal <snehalbhosale1708@gmail.com>
2024-02-16 20:56:57 +0530

    added hungry prompt

--- Hungry.java
+++ Hungry.java
@@ -1,7 +1,14 @@
 import java.util.*;
 
 public class Hungry{
+    
     public static void main(String[] args) {
-        System.out.println("I am hungry.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Are you hungry ?");
+        char c = sc.next().charAt(0);
+        if(c=='Y' || c=='y')
+            System.out.println("eat Samosa");
+        else
+            System.out.println("do coding");
     }
 }
