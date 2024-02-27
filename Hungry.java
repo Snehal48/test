77c4f71774b976ac4bc0bc45ccdbf71b21e0bff8
Snehal <snehalbhosale1708@gmail.com>
2024-02-27 14:43:42 +0530

    changes made

--- Hungry.java
+++ Hungry.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Hungry{
     
     public static void main(String[] args) {
-	 System.out.println("I am Snehal");
         Scanner sc = new Scanner(System.in);
         System.out.println("Are you hungry ?");
         char c = sc.next().charAt(0);
