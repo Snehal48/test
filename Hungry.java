14221a6dd6d397290846c2b4346fe970b8c0a445
Snehal48 <112538849+Snehal48@users.noreply.github.com>
2024-02-27 14:57:47 +0530

    removed if

--- Hungry.java
+++ Hungry.java
@@ -4,10 +4,6 @@ public class Hungry{
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Are you hungry ?");
-        char c = sc.next().charAt(0);
-        if(c=='Y' || c=='y')
-           { System.out.println("eat Samosa");
         System.out.println("Snehal");
         }
     }
